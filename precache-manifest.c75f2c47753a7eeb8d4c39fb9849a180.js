7435b910b74640527c5c438d67e6393c3cffa772
namsam28 <namsam275@gmail.com>
2020-06-25 20:00:27 +0900

    Updates

---
+++ precache-manifest.c75f2c47753a7eeb8d4c39fb9849a180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75aa8a5d7a2b3dc0d2a9175b67c2fc81",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "b126cbe58b473d8231b0",
+    "url": "/hackernews/static/js/2.7c81859f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hackernews/static/js/2.7c81859f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83d3c94c7f9f768fc02",
+    "url": "/hackernews/static/js/main.abeedde4.chunk.js"
+  },
+  {
+    "revision": "f7bf440012fddca0b2b2",
+    "url": "/hackernews/static/js/runtime-main.37644644.js"
+  }
+]);
